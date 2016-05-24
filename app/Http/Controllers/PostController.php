a34e76df834493ffe8f12e8f1d34b583fa0d29c8
Ivan Karachencev <karachencew@gmail.com>
2016-05-24 18:12:22 +0300

    Начало создания тестовых постов

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class PostController extends Controller
+{
+	public function __construct()
+	{
+	$this->middleware('auth');
+	}
+
+    public function index(Request $request)
+	{
+		return view('posts');
+	}
+
+}
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="panel panel-default">
+	<div class="panel-heading">Panel heading without title</div>
+  	<div class="panel-body">
+    	Basic panel example
+  	</div>
+</div>
+@endsection
